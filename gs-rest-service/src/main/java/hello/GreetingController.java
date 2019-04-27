4f7eb578c50dc131d4e2aee29b43f210c36eb907
Sinclair Liang <sinclairliang@gmail.com>
2019-04-27 15:31:33 -0700

    init

---
+++ gs-rest-service/src/main/java/hello/GreetingController.java
@@ -0,0 +1,24 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+// this is an annotation;
+
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting(method=GET)")
+    // ensures HTTP  requests to /greeting are mapped to the greeting() method;
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
+
